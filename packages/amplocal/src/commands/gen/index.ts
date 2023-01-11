14af0c26fed367c1c1ff276c4f4a1345e00709e3
Robert Shaw <robbynshaw@gmail.com>
2023-01-11 04:49:22 +0000

    generator directory style

--- packages/amplocal/src/commands/gen/index.ts
+++ packages/amplocal/src/commands/gen/index.ts
@@ -1,11 +1,19 @@
 import { Command, Flags } from "@oclif/core";
-import { DSGResourceData, Module } from "@amplication/code-gen-types";
+import {
+  AppInfo,
+  DSGResourceData,
+  Entity,
+  Module,
+  PluginInstallation,
+  Role,
+} from "@amplication/code-gen-types";
 import {
   createDataService,
   defaultLogger,
 } from "@amplication/data-service-generator";
-import { mkdir, readFile, writeFile } from "fs/promises";
-import { dirname, join } from "path";
+import { mkdir, readdir, readFile, writeFile } from "fs/promises";
+import path, { dirname, join } from "path";
+import { cwd } from "process";
 
 export default class Gen extends Command {
   static description = "Generate code";
@@ -17,11 +25,6 @@ generating lots of code...
   ];
 
   static flags = {
-    in: Flags.string({
-      char: "i",
-      description: "Input JSON",
-      required: true,
-    }),
     out: Flags.string({
       char: "o",
       description: "Output directory",
@@ -30,35 +33,80 @@ generating lots of code...
   };
 
   static args = [
-    // { name: "person", description: "Person to say hello to", required: true },
+    {
+      name: "directory",
+      description: "Directory containing JSON schema files",
+      required: true,
+    },
   ];
 
   async run(): Promise<void> {
     const { args, flags } = await this.parse(Gen);
 
-    const buildSpecPath = flags.in;
+    const buildSpecPath = args.directory;
     const buildOutputPath = flags.out;
 
-    // this.log(`hello ${args.person} from ${flags.from}! (./src/commands/hello/index.ts)`)
-    this.log(`generating lots of code...`);
+    this.log(`> Reading schema from directory: ${buildSpecPath}`);
+    this.log(`> Generating to output directory: ${buildOutputPath}`);
+    this.log(`\n`);
+
     await this.generateCode(buildSpecPath, buildOutputPath);
   }
 
   async generateCode(source: string, destination: string): Promise<void> {
     try {
-      const resourceData = await this.readInputJson(source);
+      const resourceData = await this.readInput(source);
+      await writeFile(
+        path.join(cwd(), "specs", "output", "test.json"),
+        JSON.stringify(resourceData, null, 2)
+      );
 
-      // const modules = await createDataService(resourceData, defaultLogger);
-      // await this.writeModules(modules, destination);
+      // todo: related field ids refer to "permanentId", whereas the other ids do not
+
+      const modules = await createDataService(resourceData, defaultLogger);
+      await this.writeModules(modules, destination);
       console.log("Code generation completed successfully");
     } catch (err) {
       console.error(err);
     }
   }
 
-  async readInputJson(filePath: string): Promise<DSGResourceData> {
+  async readInput(dir: string): Promise<DSGResourceData> {
+    const pluginFile = path.join(dir, "pluginInstallations.json");
+    const resourceFile = path.join(dir, "resourceInfo.json");
+    const entityDir = path.join(dir, "entities");
+    const roleDir = path.join(dir, "roles");
+
+    const result: DSGResourceData = {
+      resourceType: "Service",
+      resourceInfo: await this.readInputJson<AppInfo>(resourceFile, "AppInfo"),
+      entities: await this.readInputJsonDir<Entity>(entityDir, "Entity"),
+      roles: await this.readInputJsonDir<Role>(roleDir, "Role"),
+      pluginInstallations: await this.readInputJson<PluginInstallation[]>(
+        pluginFile,
+        "PluginInstallation"
+      ),
+    };
+
+    return result;
+  }
+
+  async readInputJsonDir<T>(dirPath: string, type: string): Promise<T[]> {
+    this.log(`> Finding ${type} models in '${dirPath}'...`);
+    const files = await readdir(dirPath);
+    const modelPromises = files.map(async (f) => {
+      const fPath = path.join(dirPath, f);
+      const model = await this.readInputJson<T>(fPath, type);
+      return model;
+    });
+    const results = await Promise.all(modelPromises);
+    return results;
+  }
+
+  async readInputJson<T>(filePath: string, type: string): Promise<T> {
+    this.log(`> Reading ${type} model from '${filePath}'`);
     const file = await readFile(filePath, "utf8");
-    const resourceData: DSGResourceData = JSON.parse(file);
+    const resourceData: T = JSON.parse(file);
     return resourceData;
   }
 
